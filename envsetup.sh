9c7e7fda27b6e29f2e985acbe4af723b8931da4a
Greg Baker <gregb@ifost.org.au>
2025-05-18 05:53:12 +1000

    Getting codex environment set up

---
+++ envsetup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Script to run in OpenAI codex environment. This
+# downloads everything that we need to get before
+# internet access is turned off.
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv run uvbootstrap.py
